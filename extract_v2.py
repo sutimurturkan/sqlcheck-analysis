f5132650ca4133080bb8ace2d58c31a5a6d7484f
Timurturkan, Su <gtimurturkan@gmail.com>
2020-12-12 23:48:39 -0500

    updated

--- extract_v2.py
+++ extract_v2.py
@@ -8,7 +8,7 @@ import sqlite3
 dbFilepath = 'benchmark_v1.db'
 
 def get_antipatterns(query):
-    if identify_anti_patterns_v2(query) !=[]:
+    if identify_anti_patterns_v2(query) !={}:
         ap = identify_anti_patterns_v2(query)
         print(ap)
         print("----------------")
@@ -21,8 +21,13 @@ def main():
 		sqlite_select_query = """SELECT QUERIES from SQLCheck_Benchmark_v1"""
 		cursor.execute(sqlite_select_query)
 		queries = [i[0] for i in cursor.fetchall()]
+
 		for query in queries:
-                    get_antipatterns(query)
+			try:
+				get_antipatterns(query)
+			except:
+				pass
+
 		print("Total queries are:  ", len(queries))
 
 		cursor.close()
